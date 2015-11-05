4bbdfa303f42e21839eb25ef9ed5f9f54d825e4d
Chris Dorward <chris.dorward@changingworkplace.com>
2015-11-05 14:52:06 +0000

    Company Interface

--- src/Tickergrid/ICompany.ts
+++ src/Tickergrid/ICompany.ts
@@ -1,14 +1,15 @@
 module Tickergrid
 {
-	// Company Interface
+
+    // Company Interface
     export interface ICompany
     {
         name: string;
         companyName: string;
         price: number;
         change: number;
-		changePerc: number;
-		mktCap: string;
+        changePerc: number;
+        mktCap: string;
         lastTick: string;
         lastTickTime: number;
         history: any;
